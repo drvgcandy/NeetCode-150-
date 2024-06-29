f261b0ca4e23ec88d87823bb86512c8b0b592e4e
Ramendra Raman <thegentlemonk@gmail.com>
2024-06-29 23:05:59 +0530

    Create 007.3rd max.java

---
+++ 007.3rd max.java
@@ -0,0 +1,26 @@
+/*
+Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+*/
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 1;
+        if (n<3) {
+            return nums[n-1];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] != nums[i + 1]) {
+                count++;
+            }
+            if (count==3) {
+                return nums[i];
+            }
+        }        
+        return nums[n - 1];
+    }
+}
